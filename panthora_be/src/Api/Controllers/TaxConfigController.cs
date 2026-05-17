e45f061beaf6826c27fa67a5f9bfbbd1265dc6ab
HuyNQ <ngoquochuyvn2004@gmail.com>
2026-05-17 21:47:11 +0700

    minor fix

--- panthora_be/src/Api/Controllers/TaxConfigController.cs
+++ panthora_be/src/Api/Controllers/TaxConfigController.cs
@@ -7,38 +7,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
-[Authorize(Policy = "AdminOnly")]
+
 [Route(TaxConfigEndpoint.Base)]
 public class TaxConfigController : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
         var result = await Sender.Send(new GetAllTaxConfigsQuery());
         return HandleResult(result);
     }
-
+    [AllowAnonymous]
     [HttpGet(TaxConfigEndpoint.Id)]
     public async Task<IActionResult> GetById(Guid id)
     {
         var result = await Sender.Send(new GetTaxConfigByIdQuery(id));
         return HandleResult(result);
     }
-
+    [Authorize(Policy = "AdminOnly")]
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateTaxConfigCommand command)
     {
         var result = await Sender.Send(command);
         return HandleResult(result);
     }
-
+    [Authorize(Policy = "AdminOnly")]
     [HttpPut]
     public async Task<IActionResult> Update([FromBody] UpdateTaxConfigCommand command)
     {
         var result = await Sender.Send(command);
         return HandleResult(result);
     }
-
+    [Authorize(Policy = "AdminOnly")]
     [HttpDelete(TaxConfigEndpoint.Id)]
     public async Task<IActionResult> Delete(Guid id)
     {
